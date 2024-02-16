d86579a935ca28c2f3484de49284ed13242f7e02
xsandix <157148278+xsandix@users.noreply.github.com>
2024-02-16 11:22:48 -0400

    Add files via upload

---
+++ 1.2.c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main (void)
+{
+    int A, B, C, D;
+    printf("Ingrese 4 datos de tipo entero: ");
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    printf("\n %d %d %d &d", D, C, B, A);
+}
+
+
