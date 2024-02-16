c9ee3c89bee760bedc1d9d0a271af2869ee399f0
xsandix <157148278+xsandix@users.noreply.github.com>
2024-02-16 11:56:29 -0400

    Add files via upload

---
+++ 1.8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Volumen y area del cilindro
+El programa, al recibir como datos el radio y la altura de un cilindro,
+➥calcula su área y su volumen.
+
+RAD, ALT, VOL y ARE: variables de tipo real. */
+
+int main(void)
+{
+    float RAD, ALT, VOL, ARE;
+    printf("Ingrese el radio y la altura del cilindro: ");
+    scanf("%f %f", &RAD, &ALT);
+    /*M_PI es una constante definida en math.h que contiene el valor de PI */
+    VOL = M_PI * pow(RAD, 2) * ALT;
+    ARE = 2 * M_PI * (RAD * ALT);
+    printf("\nEl volumen es: %6.2f \t El area es %6.2f", VOL, ARE);
+
+    return 0;
+}
